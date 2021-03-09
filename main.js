d231c7fea7c6a59ae47d0cbbe05db0187f6ca23b
Robert Ligman <verxus123@gmail.com>
2021-03-09 20:54:41 +0100

    refactor this maze

--- main.js
+++ main.js
@@ -8,6 +8,25 @@ const productList = [
   { name: "Prod4", price: "2223" },
 ];
 const productsInCart = [];
+const renderDivs = (item) => {
+  const div = document.createElement("div");
+  div.classList.add("product");
+  const productName = document.createElement("h3");
+  productName.classList.add("product__name");
+  productName.textContent = item.name;
+
+  const productPrice = document.createElement("p");
+  productPrice.classList.add("product__price");
+  productPrice.textContent = item.price;
+  const currency = document.createElement("span");
+  currency.classList.add("product__currency");
+  currency.textContent = "zł";
+  productPrice.appendChild(currency);
+  main.appendChild(div);
+  div.appendChild(productName);
+  div.appendChild(productPrice);
+  return div;
+};
 const renderProductsInCart = () => {
   cart.textContent = "";
   productsInCart.forEach((element) => {
@@ -20,29 +39,15 @@ const renderProductsInCart = () => {
 cartSummary.addEventListener("click", () => {
   main.innerHTML = "";
   productsInCart.forEach((item) => {
-    const div = document.createElement("div");
-    div.classList.add("product");
-    const productName = document.createElement("h3");
-    productName.classList.add("product__name");
-    productName.textContent = item.name;
-
-    const productPrice = document.createElement("p");
-    productPrice.classList.add("product__price");
-    productPrice.textContent = item.price;
-    const currency = document.createElement("span");
-    currency.classList.add("product__currency");
-    currency.textContent = "zł";
-
     const buttonDel = document.createElement("button");
     buttonDel.classList.add("product__rm-from-cart");
     buttonDel.textContent = "Usuń";
     buttonDel.addEventListener("click", (e) => {
       e.target.parentNode.remove();
     });
-    main.appendChild(div);
-    div.appendChild(productName);
-    div.appendChild(productPrice);
-    div.appendChild(buttonDel);
+    renderDivs(item).appendChild(buttonDel);
+
+    // div.appendChild(buttonDel);
   });
   console.log(
     "Wartość koszyka " +
@@ -63,27 +68,28 @@ const addToCart = (name, price) => {
   renderProductsInCart();
 };
 productList.forEach((item) => {
-  const div = document.createElement("div");
-  div.classList.add("product");
-  const productName = document.createElement("h3");
-  productName.classList.add("product__name");
-  productName.textContent = item.name;
+  // const div = document.createElement("div");
+  // div.classList.add("product");
+  // const productName = document.createElement("h3");
+  // productName.classList.add("product__name");
+  // productName.textContent = item.name;
 
-  const productPrice = document.createElement("p");
-  productPrice.classList.add("product__price");
-  productPrice.textContent = item.price;
-  const currency = document.createElement("span");
-  currency.classList.add("product__currency");
-  currency.textContent = "zł";
+  // const productPrice = document.createElement("p");
+  // productPrice.classList.add("product__price");
+  // productPrice.textContent = item.price;
+  // const currency = document.createElement("span");
+  // currency.classList.add("product__currency");
+  // currency.textContent = "zł";
 
   const buttonAdd = document.createElement("button");
+
   buttonAdd.classList.add("product__add-to-cart");
   buttonAdd.textContent = "Dodaj do koszyka";
   buttonAdd.addEventListener("click", () => addToCart(item.name, item.price));
+  renderDivs(item).appendChild(buttonAdd);
 
-  productPrice.appendChild(currency);
-  main.appendChild(div);
-  div.appendChild(productName);
-  div.appendChild(productPrice);
-  div.appendChild(buttonAdd);
+  // main.appendChild(div);
+  // div.appendChild(productName);
+  // div.appendChild(productPrice);
+  // div.appendChild(buttonAdd);
 });
